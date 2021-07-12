93251925ebc0f60756fa2f0f8fa18787825492bd
sakib <sakibsyed01@gmail.com>
2021-07-12 12:52:23 +0600

    have some problem.

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,25 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+let composeEnhancers = compose;
+// if (__DEV__) {
+//   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// }
+
+if (__DEV__) {
+  composeEnhancers =
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+    compose;
+}
+
+//const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
